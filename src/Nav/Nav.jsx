c6a161d2b9346eb2b5b1c1852ead6edff9cb829e
conrado julio <conradojuliosisnero@gmail.com>
2023-10-08 10:12:07 -0500

    menu responsive completed

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import Swich from "./Swich";
 import "./nav.css";
+import { useState } from "react";
 
 function Nav() {
-
-	const Menu = '../../img/menu.svg'
+	const Menu = "../../img/menu.svg";
 
 	//Nav-menu
 	const optionsMenu = [
@@ -15,15 +15,34 @@ function Nav() {
 		// { id: 5, name: "Contact", link: "/Contact" }
 	];
 
+	const [menu, setMenu] = useState(false);
+
+	const handelMenu = () => {
+		setMenu(!menu);
+	};
+
 	return (
 		<nav className="navegador" id="home">
-			<div className="Responsive__menu" >
-			<img className="menu__responsive" src={Menu} alt="Svg Menu" />
+			<div className="Responsive__menu">
+				<img
+					className={`menu__responsive ${menu ? "active" : ""}`}
+					src={Menu}
+					alt="Svg Menu"
+					onClick={handelMenu}
+				/>
 			</div>
-			<ul className="menu">
+			<ul className={`menu ${menu ? "menu__active" : ""}`}>
+				<img
+					src="../../img/close.svg"
+					alt="Close svg"
+					className={`close__responsive ${menu ? "active__close" : ""}`}
+					onClick={handelMenu}
+				/>
 				{optionsMenu.map((option) => (
 					<li className="lista" key={option.id}>
-						<Link className="enlaces" to={option.link}>{option.name}</Link>
+						<Link className="enlaces" to={option.link}>
+							{option.name}
+						</Link>
 					</li>
 				))}
 			</ul>
