22e4c6f4f7c68f414dd02782c433e09ba099e47e
julio cesar conrado sisnero <conradojuliosisnero@gmail.com>
2024-03-02 19:21:42 -0500

    cambios minimos

--- src/components/Folio/Folio.jsx
+++ src/components/Folio/Folio.jsx
@@ -4,7 +4,7 @@ import AboutMe from "../AboutMe/AboutMe";
 import Skills from "../Skills/Skills";
 import Nofound from "../NoFound/Nofound";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import Proyectos from "../projects/Proyectos";
+import Proyectos from "../projects/Proyectos";
 import FormNewProject from "../form/FormNewProject";
 import ProjectView from "../dashboard/ProjectView";
 
@@ -16,7 +16,7 @@ function Folio() {
         <Route path="/" element={<Presentation />} />
         <Route path="/Aboutme" element={<AboutMe />} />
         <Route path="/Skills" element={<Skills />} />
-        {/* <Route path="/Projects" element={<Proyectos />} /> */}
+        <Route path="/Projects" element={<Proyectos />} />
         <Route path="/Form" element={<FormNewProject />} />
         <Route path="/projectdatabase" element={<ProjectView />} />
         <Route path="*" element={<Nofound />} />
--- src/components/dashboard/ProjectView.jsx
+++ src/components/dashboard/ProjectView.jsx
@@ -3,7 +3,7 @@ import styles from "./projectView.module.css";
 
 export default function ProjectView() {
   // Estado de los datos del fetch de proyectos
-  const [dataprojects, setDataprojects] = useState(null);
+  const [dataprojects, setDataprojects] = useState([]);
 
   useEffect(() => {
     getDataProjects();
@@ -12,7 +12,7 @@ export default function ProjectView() {
   // Función para obtener los datos de los proyectos desde la base de datos
   const getDataProjects = async () => {
     try {
-      const response = await fetch("http://localhost:3000/projects");
+      const response = await fetch("http://localhost:3000/projectsCards");
       if (response.ok) {
         const data = await response.json();
         setDataprojects(data);
@@ -25,32 +25,30 @@ export default function ProjectView() {
   };
 
   //funcion para editar los proyectos
-  const handlerUpdate = async () => {
-    try {
-      fetch("http://localhost:3001/projects", { method: "PUT" });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const handlerUpdate = async () => {
+  //   try {
+  //     fetch("http://localhost:3001/projectsCards", { method: "PUT" });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <div className={styles.projectBox}>
       <span className="">Proyectos</span>
       {/* Mapeo de los datos de los proyectos */}
       <div className={styles.projectContend}>
-        {dataprojects?.map(({ id, name_project }) => (
+        {dataprojects?.map(({ id, name, url }) => (
           <div className={styles.cardProject} key={id}>
             <div className={styles.status}></div>
-            <span className="">{name_project}</span>
+            <a className={styles.projectName} href={url}>
+              {name}
+            </a>
             <div className={styles.buttonBoxEdit}>
-              <button className={styles.buttonEdit} onClick={handlerUpdate}>
-                Editar
-              </button>
+              <button className={styles.buttonEdit}>Editar</button>
             </div>
             <div className={styles.buttonBoxDelete}>
-              <button className={styles.buttonDelete}>
-                Eliminar
-              </button>
+              <button className={styles.buttonDelete}>Eliminar</button>
             </div>
           </div>
         ))}
