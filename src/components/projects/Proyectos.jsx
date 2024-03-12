d65175c4c67e01b067d5848e19c53ee840d8f873
julio cesar conrado sisnero <conradojuliosisnero@gmail.com>
2024-03-12 12:02:09 -0500

    update projects

--- src/components/projects/Proyectos.jsx
+++ src/components/projects/Proyectos.jsx
@@ -13,7 +13,7 @@ export default function Proyectos() {
       const response = await fetch(
         "https://65e20329a8583365b317cabe.mockapi.io/projects/Projects"
       );
-      if (response.status === 200) {  
+      if (response.status === 200) {
         const data = await response.json();
         setGetProject(data);
       } else {
@@ -42,40 +42,32 @@ export default function Proyectos() {
       </section>
 
       <div className="contend__cards-proyectos">
-        {GetProject?.map(
-          ({
-            id,
-            name,
-            img,
-            url,
-            leyend,
-            span,
-            icons
-          }) => (
-            <div className="card" id="app_movies" key={id}>
-              <div className="contend__img">
-                <img src={img} alt={name} />
-              </div>
-              <div className="contenido">
-                <h3 className="titulo_card">
-                  <a href={url}>{name}</a>
-                </h3>
-                <span className="leyenda__card">{leyend}</span>
-                <div className="iconos__card">
-                  <span>{span}</span>
-                  <div className="icon">
-                    <img src={icons.html} />
-                    <img src={icons.css} />
-                    <img src={icons.sass} />
-                    <img src={icons.js} />
-                    <img src={icons.php} />
-                    <img src={icons.sql} />
-                  </div>
+        {GetProject?.map(({ id, name, img, url, leyend, span, icons }) => (
+          <div className="card" id="app_movies" key={id}>
+            <div className="contend__img">
+              <img src={img} alt={name} />
+            </div>
+            <div className="contenido">
+              <h3 className="titulo_card">
+                <a href={url}>{name}</a>
+              </h3>
+              <span className="leyenda__card">{leyend}</span>
+              <div className="iconos__card">
+                <span>{span}</span>
+                <div className="icon">
+                  <img src={icons.html} />
+                  <img src={icons.css} />
+                  <img src={icons.sass} />
+                  <img src={icons.js} />
+                  <img src={icons.react} />
+                  <img src={icons.python} />
+                  <img src={icons.php} />
+                  <img src={icons.sql} />
                 </div>
               </div>
             </div>
-          )
-        )}
+          </div>
+        ))}
       </div>
     </section>
   );
